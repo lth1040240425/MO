a2f6f1b582f0ee6457d7b39b2aa345570833f2b9
1040240425@qq.com <1040240425@qq.com>
2017-09-05 18:19:14 +0800

    1.0.0.0

---
+++ Demo.Models.ModelConfigurations/MemberConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using MO.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models.ModelConfigurations
+{
+    public class MemberConfigurations: EntityConfigurationBase<Member,Guid>
+    {
+        public MemberConfigurations()
+        {
+            this.HasKey(p => p.Id);
+        }
+    }
+}
---
+++ Demo.Models/Member.cs
@@ -0,0 +1,48 @@
+﻿using MO.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    /// <summary>
+    /// 会员信息
+    /// </summary>
+    public class Member : EntityBase<Guid>
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string PhoneNum { get; set; }
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public DateTime Birthday { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int Gender { get; set; }
+        /// <summary>
+        /// 省份Id
+        /// </summary>
+        public Guid ProvinceId { get; set; }
+        /// <summary>
+        /// 城市Id
+        /// </summary>
+        public Guid CityId { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
